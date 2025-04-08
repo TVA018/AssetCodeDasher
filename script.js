3ab6ee4d8b808b75c0421892820ff5d275b74fb2
TVA018 <thomas.vu018@gmail.com>
2025-04-08 14:52:35 -0400

    Complete error catching

--- script.js
+++ script.js
@@ -9,13 +9,23 @@ const removeWhiteSpace = /[\s-]/;
 const inputLines = new LineList(document.getElementById("input-lines-list"));
 const outputLines = new LineList(document.getElementById("output-lines-list"), true);
 
+const catchErrorsCheck = document.getElementById("error-catch-check");
+
 statusMsg.style.color = "transparent";
 
 function addDashes(){
+    let invalidCodeLines = [];
+
     const inputs = inputLines.textArea.value.split("\n");
-    const outputs = inputs.map((inputVal) => {
+    const outputs = [...inputs.keys()].map((lineNumber) => {
+        const inputVal = inputs[lineNumber];
+
         if(inputVal.search(prefixCode) == -1){
             //not a 230-style asset code
+            if(catchErrorsCheck.checked){
+                invalidCodeLines.push(lineNumber);
+            }
+
             return inputVal;
         }
 
@@ -25,13 +35,24 @@ function addDashes(){
         return outputVal;
     });
 
+    for(let i = 0; i < inputs.length; i++){
+        const lineNumberLabel = inputLines.lineNumbers[i].HTML;
+        
+        if(invalidCodeLines.includes(i)){
+            lineNumberLabel.classList.add("error-line");
+        } else {
+            lineNumberLabel.classList.remove("error-line");
+        }
+    }
+
+    if(invalidCodeLines.length > 0){
+        alert("ERROR: Some codes in the input list (highlighted in red) are not valid 230 codes.");
+        return;
+    }
+
     statusMsg.classList.add("fading-out");
     setTimeout(() => statusMsg.classList.remove("fading-out"), 2000);
-
-    outputLines.clear();
     outputLines.setText(outputs.join("\n"));
-
-    outputLines.removeLine(0);
     
     navigator.clipboard.writeText(outputs.join("\n"));
 }
